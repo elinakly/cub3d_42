67803bd16ae898fb40683d6aac3b02f381411230
Elina Klymova <klimova.elina85@gmail.com>
2025-07-18 20:08:07 +0200

    sprite

--- srcs/map_walls_checker.c
+++ srcs/map_walls_checker.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_walls_checker.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kvalerii <kvalerii@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eklymova <eklymova@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 16:01:26 by eklymova          #+#    #+#             */
-/*   Updated: 2025/07/17 10:47:14 by kvalerii         ###   ########.fr       */
+/*   Updated: 2025/07/18 18:18:11 by eklymova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ bool	invalid_char(t_data *data)
 			if (data->map[y][x] == WALL || data->map[y][x] == WEST
 				|| data->map[y][x] == EAST || data->map[y][x] == EMPTY
 				|| data->map[y][x] == NORTH || data->map[y][x] == SOUTH
-				|| data->map[y][x] == SPACE || data->map[y][x] == SPRIT)
+				|| data->map[y][x] == SPACE || data->map[y][x] == SPRITE)
 				x++;
 			else
 				return (0);
